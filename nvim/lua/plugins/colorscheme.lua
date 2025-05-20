7674bc79d70a9ad3d3fca12fc7d1d9369925998e
takiido <takiid0@proton.me>
2025-05-20 03:29:02 -0500

    nvim

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,13 @@
+return {
+  "dgox16/oldworld.nvim",
+  lazy = false,
+  priority = 1000,
+
+  config = function()
+    require("oldworld").setup({
+      variant = "oled",
+    })
+
+    vim.cmd.colorscheme("oldworld")
+  end,
+}
