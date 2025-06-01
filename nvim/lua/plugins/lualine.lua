f84868874f7db982f01f7d0331c1e2f0e4153635
Alex Tovshchyk <takiid0@proton.me>
2025-06-01 14:43:54 -0500

    config for lualine + starship stolen from dgox16

---
+++ nvim/lua/plugins/lualine.lua
@@ -0,0 +1,15 @@
+return {
+  "hoob3rt/lualine.nvim",
+  event = "VeryLazy",
+  init = function()
+    vim.g.lualine_laststatus = vim.o.laststatus
+    if vim.fn.argc(-1) > 0 then
+      vim.o.statusline = " "
+    else
+      vim.o.laststatus = 0
+    end
+  end,
+  config = function()
+    require("config.lualine")
+  end,
+}
