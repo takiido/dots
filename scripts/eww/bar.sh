b9aad7d14a3435b533042422d65fae473c8ca820
takiido <takiid0@proton.me>
2025-09-16 18:53:12 -0500

    reworked bar run script; added volume script to eww config

--- scripts/eww/bar.sh
+++ scripts/eww/bar.sh
@@ -1,9 +1,50 @@
 #!/bin/bash
 
-# Open bar widget
-eww open bar --screen 1
+echo 1 
 
-# Pass initial screen brightess value to eww var
-~/.config/scripts/eww/helpers/brightness.sh
-# Pass initial volume value to eww var
-~/.config/scripts/eww/helpers/volume.sh
+usage() {
+    echo "Usage: $0 [-h] [-r]"
+    echo
+    echo "Options:"
+    echo "  -h            Show this help message"
+    echo "  -r            Update all variables after eww reload"
+    echo
+    echo "If no options are passed, the script will:"
+    echo "  - Create eww bar widget"
+    echo "  - Update eww variables"
+    exit 1
+}
+
+update_vars() {
+    # Pass initial screen brightess value to eww var
+    ~/.config/scripts/eww/helpers/brightness.sh
+    # Pass initial volume value to eww var
+    ~/.config/scripts/eww/helpers/volume.sh
+}
+
+reload=false
+
+# Parse options
+while getopts ":hr" opt; do
+  case $opt in
+    h)
+      usage
+      ;;
+    r)
+      reload=true
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      usage
+      ;;
+  esac
+done
+
+shift $((OPTIND -1))
+
+# Open bar widget
+if [[ "$reload" != true ]]; then
+    eww open bar --screen 1
+fi
+
+update_vars
