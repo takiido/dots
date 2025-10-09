c22410d8d686ac84234cd04612966bb9db7bf692
takiido <takiid0@proton.me>
2025-10-09 17:13:38 -0500

    updated font isntall script; added Phosphor font for icon

--- scripts/install_fonts.sh
+++ scripts/install_fonts.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 set -e
 
-FONTS_DIR="$HOME/.local/share/fonts/otf"
-SOURCE_DIR="$HOME/dots/fonts/CommitMono"
+SRC_DIR="$HOME/dots/fonts"
+DEST_DIR="$HOME/.local/share/fonts"
 
-mkdir -p "$FONTS_DIR"
+mkdir -p "$DEST_DIR"
 
-if [[ ! -d "$SOURCE_DIR" ]]; then
-  echo "Source directory $SOURCE_DIR does not exist. Exiting."
-  exit 1
-fi
-
-cp -r "$SOURCE_DIR" "$FONTS_DIR/"
+find "$SRC_DIR" -type f \( -iname "*.ttf" -o -iname "*.otf" \) | while read -r font_file; do
+  echo "Installing: $(basename "$font_file")"
+  cp "$font_file" "$DEST_DIR/"
+done
 
 fc-cache -f -v
-
