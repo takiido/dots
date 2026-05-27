7da0dd2bbc7dfd4a65f7672bb96e9252f2c52071
takiido <takiid0@proton.me>
2026-05-27 03:20:43 -0500

    cleaning up

--- ignis/modules/bar/widgets/notifications/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Notification components."""
-
-from .center import NotificationCenter
-from .popup import NotificationPopup
-from .window import NotificationWindow
-
-__all__ = ["NotificationCenter", "NotificationPopup", "NotificationWindow"]
--- ignis/modules/bar/widgets/workspaces/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Workspaces components."""
-
-from .workspaces import Workspaces
-from .workspace_btn import WorkspaceBtn
-
-__all__ = ["Workspaces", "WorkspaceBtn"]
