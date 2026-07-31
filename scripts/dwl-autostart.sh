4c0785688dc896107ab02d33f7d16130f08a42a8
takiid0 <takiid0@proton.me>
2026-07-31 12:12:48 -0500

    fixed wlr-randr bin loc

--- scripts/dwl-autostart.sh
+++ scripts/dwl-autostart.sh
@@ -4,10 +4,12 @@ pipewire &
 pipewire-pulse &
 wireplumber &
 
+WLR_DIR=/home/takiido/repos/wlr-randr/build/wlr-randr
+
 wbg -s /home/takiido/wp/dd.jpg &
 
-if wlr-randr | grep -q "ED270R"; then
-  wlr-randr --output HDMI-A-1 --mode 1920x1080@143.992996
+if WLR_DIR | grep -q "ED270R"; then
+  WLR_DIR --output HDMI-A-1 --mode 1920x1080@143.992996
 fi
 
 
