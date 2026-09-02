958a3c2fa3466514667c80773f92442466e5bdd3
takiid0 <takiid0@proton.me>
2026-09-02 09:03:16 -0500

    feat: dwl autostart script

--- scripts/dwl-autostart.sh
+++ scripts/dwl-autostart.sh
@@ -1,17 +1,25 @@
 #!/bin/sh
 
+WP=leaves.jpg
+
 pipewire &
 pipewire-pulse &
 wireplumber &
 
 WLR_DIR=/home/takiido/repos/wlr-randr/build/wlr-randr
 
-wbg -s /home/takiido/wp/dd.jpg &
+wbg -s /home/takiido/wp/$WP &
 
-if WLR_DIR | grep -q "ED270R"; then
-  WLR_DIR --output HDMI-A-1 --mode 1920x1080@143.992996
+if $WLR_DIR | grep -q "ED270R"; then
+	$WLR_DIR --output HDMI-A-1 --mode 1920x1080@143.992996
 fi
 
+pkill -x someblocks
+pkill -x somebar
 
-someblocks &
-exec somebar
+while true; do
+  pkill -x someblocks
+  rm -f "$XDG_RUNTIME_DIR"/somebar-*
+	someblocks >/tmp/someblocks.log 2>&1 &
+	somebar >/tmp/somebar.log 2>&1
+done
